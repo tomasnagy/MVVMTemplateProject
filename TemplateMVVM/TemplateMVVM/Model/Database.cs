75b82135ead18a6a3421656aa30844d60bff4fc3
Tomas Laptop <tomasngy@gmail.com>
2013-11-20 12:10:14 +0100

    Db class & references added

---
+++ TemplateMVVM/TemplateMVVM/Model/Database.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace TemplateMVVM.Model
+{
+    class Database
+    {
+        // Add reference System.Configuration
+        private static ConnectionStringSettings ConnectionString
+        {
+            get { return ConfigurationManager.ConnectionStrings["ConnectionString"]; }
+        }
+
+        private static DbConnection GetConnection()
+        {
+            DbConnection con = DbProviderFactories.GetFactory(ConnectionString.ProviderName).CreateConnection();
+            con.ConnectionString = ConnectionString.ConnectionString;
+            con.Open();
+            return con;
+        }
+
+        public static void ReleaseConnection(DbConnection con)
+        {
+            if (con != null)
+            {
+                con.Close();
+                con = null;
+            }
+        }
+
+        private static DbCommand BuildCommand(string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = GetConnection().CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = sql;
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            return command;
+        }
+
+        public static DbDataReader GetData(string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = null;
+            DbDataReader reader = null;
+
+            try
+            {
+                command = BuildCommand(sql, parameters);
+                reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (reader != null)
+                {
+                    reader.Close();
+                    if (command != null)
+                    {
+                        ReleaseConnection(command.Connection);
+                    }
+                }
+
+                throw;
+            }
+        }
+
+        public static int ModifyData(string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = null;
+
+            try
+            {
+                command = BuildCommand(sql, parameters);
+                return command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (command != null)
+                {
+                    ReleaseConnection(command.Connection);
+                    return 0;
+                }
+                throw;
+            }
+        }
+
+        public static DbParameter AddParameter(string name, object value)
+        {
+            DbParameter par = DbProviderFactories.GetFactory(ConnectionString.ProviderName).CreateParameter();
+            par.ParameterName = name;
+            par.Value = value;
+            return par;
+        }
+
+        #region Transaction
+        public static DbTransaction BeginTransaction()
+        {
+            DbConnection con = null;
+            try
+            {
+                con = GetConnection();
+                return con.BeginTransaction();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                ReleaseConnection(con);
+                throw;
+            }
+        }
+
+        private static DbCommand BuildCommand(DbTransaction trans, string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = trans.Connection.CreateCommand();
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = sql;
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            return command;
+        }
+
+        public static DbDataReader GetData(DbTransaction trans, string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = null;
+            DbDataReader reader = null;
+
+            try
+            {
+                command = BuildCommand(trans, sql, parameters);
+                command.Transaction = trans;
+                reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (reader != null)
+                {
+                    reader.Close();
+                    if (command != null)
+                    {
+                        ReleaseConnection(command.Connection);
+                    }
+                }
+
+                throw;
+            }
+        }
+
+        public static int ModifyData(DbTransaction trans, string sql, params DbParameter[] parameters)
+        {
+            DbCommand command = null;
+
+            try
+            {
+                command = BuildCommand(trans, sql, parameters);
+                command.Transaction = trans;
+                return command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (command != null)
+                {
+                    ReleaseConnection(command.Connection);
+                    return 0;
+                }
+                throw;
+            }
+        }
+        #endregion
+    }
+}
