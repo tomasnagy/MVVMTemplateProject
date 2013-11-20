af139796aef34fd4888cfcdd5fd693ca18e038c3
Tomas Laptop <tomasngy@gmail.com>
2013-11-20 12:30:33 +0100

    ViewModel-template completed

---
+++ TemplateMVVM/TemplateMVVM/ViewModel/ApplicationVM.cs
@@ -0,0 +1,49 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemplateMVVM.ViewModel
+{
+    class ApplicationVM : ObservableObject
+    {
+        public ApplicationVM()
+        {
+            //Pages.Add(new Temp1VM());
+
+            CurrentPage = Pages[0];
+
+        }
+
+        private IPage _currentpage;
+        public IPage CurrentPage
+        {
+            get { return _currentpage; }
+            set { _currentpage = value; OnPropertyChanged("CurrentPage"); }
+        }
+
+        private List<IPage> _pages;
+        public List<IPage> Pages
+        {
+            get
+            {
+                if (_pages == null)
+                    _pages = new List<IPage>();
+                return _pages;
+            }
+        }
+
+        public ICommand ChangePageCommand
+        {
+            get { return new RelayCommand<IPage>(ChangePage); }
+        }
+
+        private void ChangePage(IPage page)
+        {
+            CurrentPage = page;
+        }
+    }
+}
---
+++ TemplateMVVM/TemplateMVVM/ViewModel/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMVVM.ViewModel
+{
+    class ObservableObject
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
